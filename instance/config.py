fa774c0aa8fc53251eeda31544fe73972a3671eb
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-12-13 23:12:09 -0200

    add instance

---
+++ instance/config.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+"""
+Python Aplication Template
+Licence: GPLv3
+"""
+
+import os
+from app.libs.jsonEncoder import DateTimeEncoder
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class Config(object):
+    TESTING = os.environ.get("TESTING", False)
+
+    DATABASE_URI = "mongodb://" + os.environ.get("MAESTRO_MONGO_URI", "localhost")
+    DATABASE_NAME = os.environ.get("MAESTRO_MONGO_DATABASE", "maestro")
+    RESTFUL_JSON = {'cls': DateTimeEncoder}
+    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", 'amqp://localhost')
+
+class ProductionConfig(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
