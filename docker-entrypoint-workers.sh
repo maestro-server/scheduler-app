bbbf22166c530f530919958e1bef2bf391c04ddd
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-05-09 02:18:37 -0300

    dockerfile

--- docker-entrypoint-workers.sh
+++ docker-entrypoint-workers.sh
@@ -2,4 +2,4 @@
 
 chown -R app:app .
 rm -f celerybeat.pid
-su-exec app celery -A app.celery beat -S app.schedulers.MongoScheduler --loglevel=info
+su-exec app celery -A app.celery worker -S app.schedulers.MongoScheduler
